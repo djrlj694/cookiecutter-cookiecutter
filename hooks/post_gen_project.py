86ba808992dd22c5ea935679c4b963b2d3b823e5
djrlj694 <robert.l.jones@synthelytics.com>
2019-06-05 10:11:34 -0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = '{{ cookiecutter.lead_author }}'
+__author__ = '{{ cookiecutter.lead_name }}'
 __coauthor__ = 'N/A'
 __copyright__ = 'Copyright {% now 'local', '%Y' %}, {{ cookiecutter.repo_name }}'
-__credits__ = ['{{ cookiecutter.lead_author }}']
+__credits__ = ['{{ cookiecutter.lead_name }}']
 __license__ = '{{ cookiecutter.repo_license }}'
 __version__ = '0.0.1'
-__maintainer__ = '{{ cookiecutter.lead_author }}'
+__maintainer__ = '{{ cookiecutter.lead_name }}'
 __email__ = '{{ cookiecutter.lead_email }}'
 __status__ = 'Development'
 __created_date__= '{% now 'local', '%b %d, %Y' %}'
