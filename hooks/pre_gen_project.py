bcbb82af8a295974da1bc883f7123c04c4b0727e
djrlj694 <robert.l.jones@synthelytics.com>
2019-06-05 07:51:20 -0400

    Create pre_gen_project.py

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = '{{ cookiecutter.author }}'
+__coauthor__ = 'N/A'
+__copyright__ = 'Copyright {% now 'local', '%Y' %}, {{ cookiecutter.repo_docs_name }}'
+__credits__ = ['{{ cookiecutter.author }}']
+__license__ = '{{ cookiecutter.license }}'
+__version__ = '0.0.1'
+__maintainer__ = '{{ cookiecutter.author }}'
+__email__ = '{{ cookiecutter.email }}'
+__status__ = 'Development'
+__created_date__= '{% now 'local', '%b %d, %Y' %}'
+__modified_date__= '{% now 'local', '%b %d, %Y' %}'
+
+### Libraries ###
+
+### Variable Declarations ###
+
+### Function Declarations ###
+
+### Main Program ###
+
+sys.exit(0)
